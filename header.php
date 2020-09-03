9927522ab402975d28f1a89b545dce919f3d4589
Junyoung Heo <jyheo0@gmail.com>
2020-09-04 00:22:43 +0900

    Redirect to https

--- header.php
+++ header.php
@@ -1,4 +1,8 @@
 <?php
+  if((!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "") && $_SERVER['HTTP_HOST'] != 'localhost'){
+    header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+  }
+  
   session_start();
   if( isset( $_SESSION[ 'username' ] ) ) {
     $is_auth = TRUE;
