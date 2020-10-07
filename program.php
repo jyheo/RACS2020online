2de0b30ea6aaadf21bfcc635e6861785802fea4a
Junyoung Heo <jyheo0@gmail.com>
2020-10-07 12:26:09 +0900

    online -> virtual

--- program.php
+++ program.php
@@ -45,7 +45,7 @@
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container">
                 <img src="assets/img/SIGAPP-LOGO.jpg" height="30">&nbsp;
-                <a class="navbar-brand js-scroll-trigger" href="index.php">RACS2020 Online Conference</a>
+                <a class="navbar-brand js-scroll-trigger" href="index.php">RACS2020 Virtual Conference</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
